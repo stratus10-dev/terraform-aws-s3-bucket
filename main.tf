534bea2d7c0c6c4b8847a9ae39f2a3ee78f710be
aleksdaranutsa <aleksdaranutsa@gmail.com>
2023-01-13 15:53:27 -0800

    Removing combined count

--- main.tf
+++ main.tf
@@ -507,7 +507,7 @@ resource "aws_s3_bucket_policy" "this" {
   count = local.create_bucket && local.attach_policy ? 1 : 0
 
   bucket = aws_s3_bucket.this[0].id
-  policy = data.aws_iam_policy_document.combined[0].json
+  policy = data.aws_iam_policy_document.combined.json
 }
 
 data "aws_iam_policy_document" "combined" {
